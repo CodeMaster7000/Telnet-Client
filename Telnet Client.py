5488f1e09fa7d235d576787dc8e3da430594433b
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-08-22 22:32:39 +0530

    Create Telnet Client.py

---
+++ Telnet Client.py
@@ -0,0 +1,10 @@
+import sys
+import telnetlib
+HOST = input("Enter host: ")
+PORT = input("Enter port: ")
+telnet_obj = telnetlib.Telnet(HOST, PORT)
+message = ("GET /index.html HTTP/1.1\nHost: "+ HOST +"\n\n").encode('ascii')
+telnet_obj.write(message)
+output = telnet_obj.read_all()
+print(output)
+telnet_obj.close()
